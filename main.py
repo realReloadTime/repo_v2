0a1266b1d76341b38d40b9de6a772e3a62bfe639
Maxik <m.shilko@mail.ru>
2022-11-21 19:08:52 +0500

    version 2.0 deleted unnecessary things

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randint
 
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class Example(QMainWindow):
@@ -33,18 +33,11 @@ class Example(QMainWindow):
         self.qp.setBrush(QColor(252, 255, 1))
         side = randint(5, 200)
         self.qp.drawEllipse(randint(1, 273), randint(1, 100), side, side)
-        print('drawed')
         self.do_paint = False
-        # self.update()
-
-
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
-    sys.excepthook = except_hook
     sys.exit(app.exec())
